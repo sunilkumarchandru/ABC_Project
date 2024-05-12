57c01c8f426641cab88f0936d7dd7725e3d5bed1
sunilkumarchandru <sunilkumarchandru@gmail.com>
2024-05-12 17:27:17 +0530

    Update HelloWorld.java

--- second_directory/HelloWorld.java
+++ second_directory/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World Testing from GitHub!");
+        System.out.println("Hello, World Testing from GitHub for Devops!");
     }
 }
